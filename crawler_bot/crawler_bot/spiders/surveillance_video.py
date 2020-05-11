7f676135aca1662410ad51f0f459da8f0548ebfa
bachtuvu <bachtuvu.dev@gmail.com>
2020-05-12 00:06:22 +0700

    12052020-1

--- crawler_bot/crawler_bot/spiders/surveillance_video.py
+++ crawler_bot/crawler_bot/spiders/surveillance_video.py
@@ -9,13 +9,15 @@ class SurveillanceVideoSpider(scrapy.Spider):
     produts = CrawlerBotItem()
     name = 'surveillance_video'
     allowed_domains = ['surveillance-video.com']
-    #start_urls = ['https://www.surveillance-video.com/cameras/']
+    start_urls = ['https://www.surveillance-video.com/cameras/']
     # bullet-camera
-    start_urls = ['https://www.surveillance-video.com/bullet-cameras/shopby/h-264--/network-ip/?']
+    #start_urls = ['https://www.surveillance-video.com/bullet-cameras/shopby/h-264--/network-ip/?']
     # dome-camera
     #start_urls = ['https://www.surveillance-video.com/dome-cameras/shopby/network-ip/?']
     # ptz-camera
     #start_urls = ['https://www.surveillance-video.com/ptz-cameras/shopby/network-ip/']
+
+    
 
     def parse(self, response):
             links = response.css("div.productdescpart a::attr(href)").extract()
@@ -38,7 +40,10 @@ class SurveillanceVideoSpider(scrapy.Spider):
 
     
     def parse_detail_page(self, response):
-       
+        
+        th_table = []
+        td_table = []
+        product_specs = []    
         product = response.meta['product']
         
         title = response.css('div.product-name  h1::text').get()
@@ -48,66 +53,75 @@ class SurveillanceVideoSpider(scrapy.Spider):
         price = response.xpath('//div[contains(@class, "price-box")]//span//span//span/text()').get()
         product_detail_p = response.css("div#description_tabbed div.std p::text").extract()
         product_detail_li = response.css("div#description_tabbed div.std ul li::text").extract()
-        product_detail = response.css("div#description_tabbed div.std").extract()
-        product_specs_th = list(dict.fromkeys(response.css("div#additional_tabbed table#product-attribute-specs-table tbody tr th::text").extract()))
-        product_specs = response.css("div#additional_tabbed table#product-attribute-specs-table").extract()
+        product_detail = product_detail_p + product_detail_li
+        #product_detail = response.css("div#description_tabbed div.std").extract()
+        #product_specs_th = list(dict.fromkeys(response.css("div#additional_tabbed table#product-attribute-specs-table tbody tr th::text").extract()))
+        for row in response.css("div#additional_tabbed table#product-attribute-specs-table tbody tr"):
+            label = row.css("th.label::text").get().replace("\xa0","")
+            text = row.css("td::text").get()
+            merge = label + ":" + text
+            th_table.append(label)
+            td_table.append(text)
+            product_specs.append(merge)
+                
+        #product_specs = response.css("div#additional_tabbed table#product-attribute-specs-table").extract()
         features_common = response.css("div#additional_tabbed table#product-attribute-specs-table tbody tr td::text").getall()
         #tr_lst = response.css("div#additional_tabbed table#product-attribute-specs-table tbody tr").extract()
             
 
         try:
-            # manufacturer_id = label.index('Manufacturer')
-            manufacturer = features_common[1]
+            manufacturer_id = th_table.index('Manufacturer')
+            manufacturer = td_table[manufacturer_id]
         except ValueError as e:
             manufacturer = ''
 
         try:
-            #  features_id = label.index('Features')
-             features = features_common[2]
+             features_id = th_table.index('Features')
+             features = td_table[features_id]
         except ValueError as e:
              features = ''
 
         try:
-            #  camera_signal_id = label.index('Camera Signal')
-             camera_signal = features_common[3]
+             camera_signal_id = th_table.index('Camera Signal ')
+             camera_signal = td_table[camera_signal_id]
         except ValueError as e:
              camera_signal = ''
 
-        # try:
-        #     resolution_id = label.index('Resolution')
-        #     resolution = features_common[resolution_id]
-        # except ValueError as e:
-        #     resolution = ''
+        try:
+            resolution_id = th_table.index('Resolution')
+            resolution = td_table[resolution_id]
+        except ValueError as e:
+            resolution = ''
 
-        # try:
-        #     infrared_distance_id = label.index('Infrared Distance')
-        #     infrared_distance = features_common[infrared_distance_id]
-        # except ValueError as e:
-        #     infrared_distance = ''
+        try:
+            infrared_distance_id = th_table.index('Infrared Distance')
+            infrared_distance = td_table[infrared_distance_id]
+        except ValueError as e:
+            infrared_distance = ''
 
-        # try:
-        #     sensor_option_id = label.index('Sensor Option')        
-        #     sensor_option = features_common[sensor_option_id]
-        # except ValueError as e:
-        #     sensor_option = ''
+        try:
+            sensor_option_id = th_table.index('Sensor Option')        
+            sensor_option = td_table[sensor_option_id]
+        except ValueError as e:
+            sensor_option = ''
 
-        # try:
-        #     lens_type_id = label.index('Lens Type')
-        #     lens_type = features_common[lens_type_id]
-        # except ValueError as e:
-        #     lens_type = ''
+        try:
+            lens_type_id = th_table.index('Lens Type')
+            lens_type = td_table[lens_type_id]
+        except ValueError as e:
+            lens_type = ''
 
-        # try:
-        #     lens_size_id = label.index('Lens Size')
-        #     lens_size = features_common[lens_size_id]
-        # except ValueError as e:
-        #     lens_size = ''
+        try:
+            lens_size_id = th_table.index('Lens Size')
+            lens_size = td_table[lens_size_id]
+        except ValueError as e:
+            lens_size = ''
 
-        # try:  
-        #     durability_id = label.index('Durability')
-        #     durability = features_common[durability_id]
-        # except ValueError as e:
-        #     durability = ''
+        try:  
+            durability_id = th_table.index('Durability')
+            durability = td_table[durability_id]
+        except ValueError as e:
+            durability = ''
 
 
         loader_detail = ItemLoader(item=product, response = response)
@@ -117,12 +131,12 @@ class SurveillanceVideoSpider(scrapy.Spider):
         loader_detail.add_value('manufacturer', manufacturer)
         loader_detail.add_value('features', features)
         loader_detail.add_value('camera_signal', camera_signal)
-        #loader_detail.add_value('resolution', resolution)
-        #loader_detail.add_value('sensor_option', sensor_option)
-        #loader_detail.add_value('lens_type', lens_type)
-        #loader_detail.add_value('lens_size', lens_size)
-        #loader_detail.add_value('infrared_distance', infrared_distance)
-        #loader_detail.add_value('durability', durability)
+        loader_detail.add_value('resolution', resolution)
+        loader_detail.add_value('sensor_option', sensor_option)
+        loader_detail.add_value('lens_type', lens_type)
+        loader_detail.add_value('lens_size', lens_size)
+        loader_detail.add_value('infrared_distance', infrared_distance)
+        loader_detail.add_value('durability', durability)
         loader_detail.add_value('price', price)
         loader_detail.add_value('product_detail', product_detail)
         loader_detail.add_value('product_specs', product_specs)
